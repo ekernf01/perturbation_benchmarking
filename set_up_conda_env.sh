b4c24102433eaec977cd2cca8e8c3270a834cc15
Eric Kernfeld <eric.kern13@gmail.com>
2022-03-31 09:46:22 -0400

    Finish versions 2 to 4. Version 4 gives an interesting signal.

---
+++ set_up_conda_env.sh
@@ -0,0 +1,9 @@
+conda create -n cell_type_grn_transfer python=3.9
+conda activate  cell_type_grn_transfer
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda install numba cython pybedtools jupyter jupyter-lab pysam fa2 
+conda install -c conda-forge leidenalg
+pip3 install git+https://github.com/morris-lab/CellOracle.git
+conda install scanpy
