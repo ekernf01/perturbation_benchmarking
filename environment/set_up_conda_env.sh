4d4295363b0aa256aba83ce49b5bd8cefdfa903c
Gary Yang <46495852+GaryCloudY@users.noreply.github.com>
2022-10-28 14:13:06 -0400

    Update set_up_conda_env.sh
    Specify 0.8.10 for leidenalg because 0.8.x leidenalg uses 0.9.x igraph and 0.9.x leidenalg uses 1.0.x igraph; however, cellOracle 0.10.12 only works 0.9.x igraph

--- environment/set_up_conda_env.sh
+++ environment/set_up_conda_env.sh
@@ -4,7 +4,7 @@ conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 conda install numba cython pybedtools jupyter jupyter-lab pysam fa2 
-conda install -c conda-forge leidenalg
+conda install -c conda-forge leidenalg=0.8.10
 pip3 install git+https://github.com/morris-lab/CellOracle.git
 conda install scanpy
 conda install dask
