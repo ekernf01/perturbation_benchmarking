f67e2d1d758860a46f9f7916abbd0279aeb26607
Eric Kernfeld <eric.kern13@gmail.com>
2022-11-02 15:08:09 -0400

    Clean up messy misc files that are unfortunately already tracked

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,11 +1,14 @@
-conda activate cell_type_grn_transfer
-for experiment in `ls experiments -1` 
-do
-    bash -c "exec -a $experiment python src/experimenter.py --experiment_name $experiment --amount_to_do models" \
-        > experiments/$experiment/out.txt 2> experiments/$experiment/err.txt 
-done
 
 echo "Useful commands:
 wc -l experiments/*/err.txt
 wc -l experiments/*/out.txt
 "
+conda activate cell_type_grn_transfer
+for experiment in `ls experiments -1` 
+do
+    echo "Starting ${experiment}"
+    python -m \
+        cProfile -o experiments/$experiment/profiling.txt \
+        src/experimenter.py --experiment_name $experiment --amount_to_do models \
+        > experiments/$experiment/out.txt 2> experiments/$experiment/err.txt
+done
