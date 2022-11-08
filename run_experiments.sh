c488bb1cfaf828747679997ca40c05d15f48c75a
Eric Kernfeld <eric.kern13@gmail.com>
2022-11-08 15:33:12 -0500

    Update wrapper bash script

--- run_experiments.sh
+++ run_experiments.sh
@@ -7,8 +7,9 @@ conda activate cell_type_grn_transfer
 for experiment in `ls experiments -1` 
 do
     echo "Starting ${experiment}"
-    python -m \
-        cProfile -o experiments/$experiment/profiling.txt \
-        src/experimenter.py --experiment_name $experiment --amount_to_do models \
+    # python -m memory_profiler 
+    # mprof run --include-children --multiprocess 
+    # python -m cProfile -o experiments/$experiment/profiling.txt \
+    python src/do_one_experiment.py --experiment_name $experiment --amount_to_do plots \
         > experiments/$experiment/out.txt 2> experiments/$experiment/err.txt
 done
