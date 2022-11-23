d7037503ba5f1bc79463d02cdb215ef819ccfa60
Eric Kernfeld <eric.kern13@gmail.com>
2022-11-23 09:37:35 -0500

    Fix leidenalg install

--- environment/set_up_conda_env.sh
+++ environment/set_up_conda_env.sh
@@ -4,10 +4,9 @@ conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 conda install numba cython pybedtools jupyter jupyter-lab pysam fa2 
-conda install -c conda-forge leidenalg=0.8.10
-pip3 install git+https://github.com/morris-lab/CellOracle.git
-conda install scanpy
+conda install -c conda-forge scanpy python-igraph leidenalg
 conda install -c conda-forge altair vega_datasets altair_saver
 conda install pyarrow
 conda install -c conda-forge python-duckdb
-
+conda install -n cell_type_grn_transfer ipykernel --update-deps --force-reinstall
+conda install regex
