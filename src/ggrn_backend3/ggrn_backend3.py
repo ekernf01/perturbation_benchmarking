b980a6576db0bb23fe7a4fd92fe15e9ec7515013
Eric Kernfeld <eric.kern13@gmail.com>
2022-12-22 09:53:44 -0500

    Pass first unit test of ggrn beckend3

--- src/ggrn_backend3/ggrn_backend3.py
+++ src/ggrn_backend3/ggrn_backend3.py
@@ -1,34 +1,135 @@
 import torch
 import pytorch_lightning as pl
-import linear
+import ggrn_backend3.linear
+import anndata
+import math
+import pandas as pd
+import numpy as np
 
-class AutoregressiveModel:
-    def __init__():
+class AnnDataSetMatchedControls(torch.utils.data.Dataset):
+    def __init__(self, adata: anndata.AnnData, matching_method: str) -> None:
+        super().__init__()
+        self.adata = adata
+        self.adata.var["numeric_index"] = [i for i in range(self.adata.var.shape[0])]
+        def apply_over_comma_separated_string(s: str, f, sep=","):
+            return sep.join([str(f(x)) for x in s.split(sep)])
+        self.adata.obs["perturbation_index"] = [
+            apply_over_comma_separated_string(p, self.get_index_from_gene)
+            for p in self.adata.obs["perturbation"]
+        ]
+
+        # Mark each observation as treatment, control, and/or steady state
+        if "is_control" not in set(self.adata.obs.columns):
+            raise KeyError("train data must have a boolean column in .obs with key 'is_control'.")
+        if self.adata.obs["is_control"].dtype.name != "bool":
+            dt = self.adata.obs["is_control"].dtype
+            raise KeyError(f"train.obs['is_control'] must be boolean. dtype: {repr(dt)}")
+        if "is_treatment" not in set(self.adata.obs.columns):
+            self.adata.obs["is_treatment"] = ~self.adata.obs["is_control"]
+        if "is_steady_state" not in set(self.adata.obs.columns):
+            self.adata.obs["is_steady_state"] = self.adata.obs["is_control"]
+        if "perturbation" not in self.adata.obs.columns:
+            raise KeyError("train data must have a comma-separated str column in .obs with key 'perturbation'.")
+        if "expression_level_after_perturbation" not in self.adata.obs.columns:
+            raise KeyError("train data must have a comma-separated str column in .obs with key 'expression_level_after_perturbation'.")
+
+        self.adata = MatchControls(self.adata, matching_method)
+
+    def __len__(self):
+        return self.adata.obs.shape[0]
+
+    def __getitem__(self, idx):
+        matched_idx = self.adata.obs["matched_control"][idx]
+        return {
+            "treatment":      {
+                "expression": self.adata.X[idx,        :], 
+                "metadata": self.adata.obs.iloc[idx,:].to_dict(),
+            },
+            "matched_control":{
+                "expression":        self.adata.X[matched_idx,:],
+                "metadata":   self.adata.obs.iloc[matched_idx,:].to_dict(),
+            },
+        }
+    
+    def get_index_from_gene(self, g):
+        return self.adata.var.loc[g, "numeric_index"] if g in self.adata.var_names else -999 #sentinel value indicates this is a control
+
+def MatchControls(train_data: anndata.AnnData, matching_method: str):
+    if matching_method == "closest":
+        raise NotImplementedError("Cannot yet match to closest control.")
+        train_data.obs["matched_control"] = "placeholder"
+    elif matching_method == "random":
+        train_data.obs["matched_control"] = np.random.choice(
+            np.where(train_data.obs["is_control"])[0], 
+            train_data.obs.shape[0], 
+            replace = True,
+        )
+    elif matching_method == "user":
+        assert "matched_control" in train_data.obs.columns
+    else: 
+        raise ValueError("matching method must be 'closest' or 'random'.")
+    return train_data
+
+def kaiming_init(model):
+    """
+    Initialization suitable for use with leaky ReLU, from Kaiming He et al 2015.
+    From the official Pytorch website, https://pytorch-lightning.readthedocs.io/en/stable/notebooks/course_UvA-DL/03-initialization-and-optimization.html
+    Author: Phillip Lippe
+    License: CC-by-SA
+    """
+    for name, param in model.named_parameters():
+        if name.endswith(".bias"):
+            param.data.fill_(0)
+        elif name.startswith("layers.0"):  # The first layer does not have ReLU applied on its input
+            param.data.normal_(0, 1 / math.sqrt(param.shape[1]))
+        else:
+            param.data.normal_(0, math.sqrt(2) / math.sqrt(param.shape[1]))
+
+class GGRNAutoregressiveModel:
+
+    def __init__(self, train_data, matching_method):
+        self.train_data = AnnDataSetMatchedControls(train_data, matching_method)
         self.model = None
         return
+
     def train(
-        train_data,
-        is_treatment = None, # defaults to ~train_data.obs["is_control"]
-        is_control   = None, # defaults to train_data.obs["is_control"]
-        is_steady_state = None, # defaults to is_control
-        matching = "closest",
+        self,
         S = 1,
         regression_method = "linear",
         low_dimensional_structure = "none",
         low_dimensional_training = "PCA",
         network = None,
         device = None,
+        limit_train_batches = 100,
+        max_epochs = 1, 
+        learning_rate = 0.001,
+        batch_size = 64,
+        regularization_parameter = 1,
     ):
         if device is None:
             device = "cuda" if torch.cuda.is_available() else "cpu"
-        # TODO: make dataloader
-        # TODO: select appropriate model architecture
-        self.model = linear.LinearAutoregressive()
-        # TODO: run optimizer        
+        self.model = ggrn_backend3.linear.LinearAutoregressive(
+            n_genes = self.train_data.adata.X.shape[1],
+            S = S,
+            regression_method = regression_method,
+            low_dimensional_structure = low_dimensional_structure,
+            low_dimensional_training = low_dimensional_training,
+            learning_rate = learning_rate, 
+            regularization_parameter = regularization_parameter,
+        )
+        kaiming_init(self.model)
+
+        dataloader = torch.utils.data.DataLoader(self.train_data, batch_size=batch_size, shuffle=True)
+        trainer = pl.Trainer(limit_train_batches=limit_train_batches, max_epochs=max_epochs, accelerator=device)
+        trainer.fit(model=self.model, train_dataloaders=dataloader)
         return 
         
-    def predict(example_perturbations):
+    def predict(
+        self, 
+        example_perturbations, 
+        ):
         # TODO: select controls
         # TODO: run model forwards
-        self.model()
+        # self.model()
+        return
 
--- src/test_ggrn_backend3.py
+++ src/test_ggrn_backend3.py
@@ -1,32 +1,58 @@
-PROJECT_PATH = '/home/ekernf01/Desktop/jhu/research/projects/perturbation_prediction/cell_type_knowledge_transfer/'
-import sys 
 import os
-import ggrn_backend3.ggrn_backend3
+try:
+    PROJECT_PATH = '/home/ekernf01/Desktop/jhu/research/projects/perturbation_prediction/cell_type_knowledge_transfer/'
+    os.chdir(os.path.join(PROJECT_PATH, "perturbation_benchmarking"))
+except FileNotFoundError:
+    PROJECT_PATH = 'gary_path'
+    os.chdir(os.path.join(PROJECT_PATH, "perturbation_benchmarking"))
+
+import sys 
 import unittest
+sys.path.append(os.path.expanduser(os.path.join(PROJECT_PATH, 'perturbation_benchmarking', 'src'))) 
 sys.path.append(os.path.expanduser(os.path.join(PROJECT_PATH, 'network_collection', 'load_networks'))) 
+import ggrn_backend3.ggrn_backend3
 import load_networks
 import scanpy as sc
+import numpy as np
+import torch
 
-class TestModelRuns(unittest.TestCase):
+train_data = sc.read_h5ad("../accessory_data/nakatake.h5ad")
+network = load_networks.LightNetwork(files=["../accessory_data/human_promoters.parquet"])
+example_perturbations = (("KLF8", 0), ("GATA1", 0), ("empty_vector", np.nan))
+
+class TestBackend3(unittest.TestCase):
+    def test_matching_and_loading(self):
+        td = ggrn_backend3.ggrn_backend3.MatchControls(train_data, "random")
+        # td = ggrn_backend3.ggrn_backend3.MatchControls(train_data, "closest")
+        torchy_dataset = ggrn_backend3.ggrn_backend3.AnnDataSetMatchedControls(td, "random")
+        torchy_dataloader = torch.utils.data.DataLoader(torchy_dataset)
+        x = torchy_dataset[0]
+        batch = next(iter(torchy_dataloader))
+        assert "treatment" in x.keys()
+        assert "matched_control" in x.keys()
+        assert "expression" in x["treatment"].keys()
+        assert "metadata"   in x["treatment"].keys()
+        assert "expression" in x["matched_control"].keys()
+        assert "metadata"   in x["matched_control"].keys()
+        assert all(
+            [f in x["treatment"]["metadata"].keys() 
+            for f in {"is_control", "is_treatment", "is_steady_state", "perturbation_index", "expression_level_after_perturbation"}]
+        )
+
     def test_user_interface(self):
-        train_data = sc.read_h5ad("../accessory_data/nakatake.h5ad")
-        network = load_networks.LightNetwork(files=["../accessory_data/human_promoters.parquet"])
-        example_perturbations = (("KLF8", 0), ("GATA1", 0), ("empty_vector", np.nan))
-        for matching in "random", "closest":
-            for regression_method in "linear", "multilayer_perceptron":
-                for low_dimensional_structure in "none", "QiGQ":
-                    if low_dimensional_structure == "QiGQ":
+        for matching in [
+            "random", 
+            # "closest",
+        ]:
+            for regression_method in ["linear"]: #, "multilayer_perceptron"]:
+                for low_dimensional_structure in ["none"]: #, "RGQ"]:
+                    if low_dimensional_structure == "RGQ":
                         low_dimensional_trainings = ["supervised", "PCA", "fixed"]
                     else:
-                        low_dimensional_trainings = ["this_is_ignored"]
+                        low_dimensional_trainings = [None]
                     for low_dimensional_training in low_dimensional_trainings:
-                        model = ggrn_backend3.AutoregressiveModel()
+                        model = ggrn_backend3.ggrn_backend3.GGRNAutoregressiveModel(train_data, matching_method = "random")
                         model.train(
-                            train_data,
-                            is_treatment = None, # defaults to ~train_data.obs["is_control"]
-                            is_control   = None, # defaults to train_data.obs["is_control"]
-                            is_steady_state = None, # defaults to is_control
-                            matching = matching,
                             S = 1,
                             regression_method = regression_method,
                             low_dimensional_structure = low_dimensional_structure,
