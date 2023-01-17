c2bf61ae2643f3e5d88e4ddf5941a77b1ca41c79
Eric Kernfeld <eric.kern13@gmail.com>
2023-01-17 11:45:09 -0500

    tidy repo

--- run_experiments.sh
+++ run_experiments.sh
@@ -7,8 +7,7 @@ conda activate cell_type_grn_transfer
 for experiment in `ls experiments -1` 
 do
     echo "Starting ${experiment}"
-    # python -m memory_profiler 
-    # mprof run --include-children --multiprocess
     python src/do_one_experiment.py --experiment_name $experiment --amount_to_do missing_models --save_trainset_predictions \
-        > experiments/$experiment/out.txt 2> experiments/$experiment/err.txt 
+        > experiments/$experiment/out.txt 2> experiments/$experiment/err.txt
+
 done
