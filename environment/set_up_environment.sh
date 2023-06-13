fc0fa78e385dc8b664cffa383f95b0d79127e32b
Eric Kernfeld <eric.kern13@gmail.com>
2023-06-13 09:51:26 -0400

    Tidy up environment-related files

--- environment/set_up_environment.sh
+++ environment/set_up_environment.sh
@@ -3,12 +3,13 @@
 # Get mamba
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
 bash Mambaforge-$(uname)-$(uname -m).sh -b
-bash #restart shell for mamba to be available
+source "${HOME}/mambaforge/etc/profile.d/conda.sh"
 
 # Set up Conda env
 git clone http://github.com/ekernf01/perturbation_benchmarking #currently private
 cd perturbation_benchmarking
-mamba create --name ggrn --file spec_file.yaml
+# If you have a GPU, you can use conda_list_explicit_gpu.txt.
+mamba create --name ggrn --file conda_list_explicit.txt
 conda activate ggrn
 pip install cell-gears==0.0.4  --no-deps
 pip install celloracle==0.12.0 --no-deps
@@ -28,15 +29,24 @@ done
 git clone https://huggingface.co/ctheodoris/Geneformer
 cd Geneformer
 pip install .
-
+cd ..
 
 # Get data collections
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-aws configure #then put in pass and stuff. Eventually this should be a Zenodo download or something public.
-for d in network_collection perturbation_data accessory_data
+
+# For now, get data from AWS S3
+# Eventually this should be a Zenodo download or something public.
+# Note to self: if you sync perturbation_benchmarking, then you put all local results onto this ec2 instance.
+aws configure 
+for d in network_collection perturbation_data accessory_data perturbation_benchmarking 
 do
     mkdir $d
     aws s3 sync s3://cahanlab/eric.kernfeld/eric_laptop/research/projects/perturbation_prediction/cell_type_knowledge_transfer/$d/ $d
 done
+# My AWS backups have old files too, not just current ones. These can cause issues. 
+rm ../network_collection/networks/*/networks/*.csv.gz 
+
+# To move results back to EK laptop via Patrick s3
+aws s3 sync perturbation_benchmarking/experiments s3://cahanlab/eric.kernfeld/eric_laptop/research/projects/perturbation_prediction/cell_type_knowledge_transfer/perturbation_benchmarking/experiments
