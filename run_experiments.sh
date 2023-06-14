877b1657c06a4c4d71403936e35998c1b868b146
Eric Kernfeld <eric.kern13@gmail.com>
2023-06-14 11:09:25 -0400

    Prepare to run dcdfg on other data

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+source "${HOME}/mambaforge/etc/profile.d/conda.sh"
 conda activate ggrn
-for experiment in 1.9_1 # 1.2.2_1 1.2.2_2 1.2.2_3 
+for experiment in 1.6.1_7 1.6.1_8 1.6.1_9 1.6.1_10 1.6.1_11
 do
     echo "Starting ${experiment}"
     echo "Monitor progress:
@@ -8,4 +10,8 @@ less experiments/${experiment}/stdout.txt
 "
     python do_one_experiment.py --experiment_name $experiment --amount_to_do missing_models --save_trainset_predictions \
         > experiments/$experiment/stdout.txt 2> experiments/$experiment/err.txt
+
+    # To move results back to EK laptop via s3
+    echo "aws s3 sync experiments/${experiment} s3://cahanlab/eric.kernfeld/eric_laptop/research/projects/perturbation_prediction/cell_type_knowledge_transfer/perturbation_benchmarking/experiments/${experiment}"
+    echo "aws s3 sync s3://cahanlab/eric.kernfeld/eric_laptop/research/projects/perturbation_prediction/cell_type_knowledge_transfer/perturbation_benchmarking/experiments/${experiment} experiments/${experiment}"
 done
