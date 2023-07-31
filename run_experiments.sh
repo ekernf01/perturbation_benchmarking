85edadddc6f950a35ebde89bdf4c75d70d900987
Eric Kernfeld <eric.kern13@gmail.com>
2023-07-31 07:48:08 -0400

    Update run_experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,6 +1,6 @@
 source "${HOME}/mambaforge/etc/profile.d/conda.sh"
 conda activate ggrn
-for experiment in `ls experiments -1 | grep '1.4.3_'`
+for experiment in  `ls -1 experiments` 
 do
     echo "Starting ${experiment}"
     echo "Monitor progress:
@@ -9,8 +9,4 @@ less experiments/${experiment}/stdout.txt
 "
     python do_one_experiment.py --experiment_name $experiment --amount_to_do missing_models --save_trainset_predictions \
         > experiments/$experiment/stdout.txt 2> experiments/$experiment/err.txt
-
-    # To move results back to EK laptop via s3
-    echo "aws s3 sync experiments/${experiment} s3://cahanlab/eric.kernfeld/eric_laptop/research/projects/perturbation_prediction/cell_type_knowledge_transfer/perturbation_benchmarking/experiments/${experiment}"
-    echo "aws s3 sync s3://cahanlab/eric.kernfeld/eric_laptop/research/projects/perturbation_prediction/cell_type_knowledge_transfer/perturbation_benchmarking/experiments/${experiment} experiments/${experiment}"
 done
