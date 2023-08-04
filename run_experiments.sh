3e47d168339937ff50d33c86cddf4136acd3f2c5
Eric Kernfeld <eric.kern13@gmail.com>
2023-08-04 08:49:06 -0400

    choose experiments to run on aws

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,6 +1,7 @@
 source "${HOME}/mambaforge/etc/profile.d/conda.sh"
 conda activate ggrn
-for experiment in  `ls -1 experiments | grep 1.0_*` 
+for experiment in  "1.4.2_1", "1.4.2_2", "1.4.2_3", "1.4.2_5", "1.4.2_6", "1.4.2_7"
+#`ls -1 experiments | grep 1.4.2_*` 
 do
     echo "Starting ${experiment}"
     echo "Monitor progress:
