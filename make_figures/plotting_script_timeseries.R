d48add0f33c6301e08ce045079176c1285fd51d8
Eric Kernfeld <eric.kern13@gmail.com>
2025-01-13 09:06:22 -0500

    Update timeseries mscrpt plots

--- make_figures/plotting_script_timeseries.R
+++ make_figures/plotting_script_timeseries.R
@@ -324,7 +324,8 @@
     write.csv("timeseries_plots/mouse_blood_ps_vs_screen_top_30.csv")
 }
 
-# ======================== Figure 2: evaluation of log FC =====================================
+# ======================== Figures 2 and 4: evaluation of log FC =====================================
+# timescale experiments
 {
   METRICS = c(METRICS, "distance_in_pca")
   X = collect_experiments(paste0("1.2.2_", 16:21)) %>% make_the_usual_labels_nice
@@ -344,100 +345,28 @@
   print(plot)
   ggsave('timeseries_plots/matching.svg', plot, width = 8, height = 8)
 }
-
-
-# =========================== Figure 3: cell type-specific networks ===============================
+# published methods
 {
-  # Compare perturbation scores versus CRISPR screen
-  embeddings = load_embeddings("1.4.1_0")
-  lit_review = read.csv("../../perturbation_data/perturbations/definitive_endoderm/lit_review.csv")
-  embeddings %<>% merge(lit_review, by = "perturbation", all.x = T)
-  overall_scores = embeddings %>%
-    group_by(cell_type, perturbation, prediction_timescale, matching_method) %>% # out of date
-    summarize(
-      perturbation_score = mean(perturbation_score), 
-      li_et_al_biggest_effect = maxabs(c(
-        mean(brunello.neg.lfc),
-        mean(brunello.pos.lfc),
-        mean(gecko.neg.lfc),
-        mean(gecko.pos.lfc)
-      )),
-      Included.in.literature.review. = unique(Included.in.literature.review.), 
-      Cell.type.affected = unique(Cell.type.affected),
-      PMID = unique(PMID),
-      Notes = unique(Notes)
-    ) %>%
-    group_by(cell_type, prediction_timescale, matching_method) %>%
-    mutate( perturbation_score_rank = rank(-abs(perturbation_score)) ) %>%
-    mutate( top_30 = ifelse(perturbation_score_rank<=30, "top 30 predictions", "other") ) %>%
-    subset(cell_type != "pluripotent") 
-  
-  ggplot(overall_scores) + 
-    facet_grid(~cell_type, scale = "free_x") +
-    geom_hline(yintercept=0) +
-    geom_vline(xintercept=0) +
-    geom_point(aes(perturbation_score, li_et_al_biggest_effect, color = matching_method, alpha = top_30)) + 
-    ggrepel::geom_text_repel(
-      data=subset(overall_scores, perturbation_score_rank<=5) %>% 
-        group_by(perturbation, cell_type) %>%
-        summarise(perturbation_score = maxabs(perturbation_score), 
-                  li_et_al_biggest_effect = li_et_al_biggest_effect[[1]]),
-      mapping = aes(perturbation_score, li_et_al_biggest_effect, label = perturbation)
-    ) +
-    xlab("Cell type-specific perturbation score") +
-    ylab("Log fold change in gRNA abundance \n(Largest magnitude from Li et al. 2019 genome-wide screen)") + 
-    ggtitle("Predicted and observed effects on endoderm differentiation",
-            subtitle = "Labels are shown for the largest absolute values on the x axis.")
-  ggsave("timeseries_plots/definitive_endoderm_ps_vs_screen.pdf", width = 8, height = 6)
-}
-{
-  X = collect_experiments(paste0("1.4.1_", 0:4))
-  X %<>% subset(is_timescale_strict)
-  X[X[["pearson_top_20"]]!=0, ]
-  X %<>% add_network_cell_type_metadata
-  X %<>% make_the_usual_labels_nice
-  X %<>% subset(is_timescale_strict)
-  X %<>% subset(prediction_timescale==10)
-  X$pearson %<>% tidyr::replace_na(0)
-  X$pearson_top_20 %<>% tidyr::replace_na(0)
-  X$pearson_top_100 %<>% tidyr::replace_na(0)
-  X$pearson_top_200 %<>% tidyr::replace_na(0)
+  METRICS = c(METRICS, "distance_in_pca")
+  X = collect_experiments(paste0("1.5.1_", 0:1)) %>% make_the_usual_labels_nice
   X %<>%
-    group_by(prediction_timescale, perturbation_dataset, network_datasets, network_source, network_tissue, network_pretty, network_cell_type_matches ) %>%
-    summarise(across(DEFAULT_METRICS, mean, na.rm = T))
-  X %<>% tidyr::pivot_longer(cols = all_of(DEFAULT_METRICS), names_to = "metric")
+    group_by(regression_method, perturbation_dataset) %>%
+    summarise(across(METRICS, mean, na.rm = T))
+  X %<>% tidyr::pivot_longer(cols = all_of(METRICS), names_to = "metric")
   X[["metric"]] %<>% gsub("_", " ", .)
-  X[["metric"]] %<>% factor(levels = gtools::mixedsort(unique(X[["metric"]])))
-  X %<>% subset(network_source != "humanbase")
-  X$source = X$network_source %>% 
-    gsub("_tissue", "", . ) %>% 
-    gsub("cellnet_human.*", "cellnet_human", .) %>% 
-    gsub("cellnet_mouse.*", "cellnet_mouse", .)
-  for(mymetric in c("overlap top 20", "pearson top 20")){
-    plot = X %>% 
-      subset(metric == mymetric) %>%
-      ggplot() + 
-      geom_bar(
-        aes(
-          x = network_pretty, 
-          y = value, 
-          color = source, 
-          fill = c("Different", "Matched")[network_cell_type_matches+1]
-        ), 
-        stat = "identity", 
-        position = "dodge") +
-      scale_fill_manual(values = c("gray", "black")) +
-      labs(fill = "Network cell type matches \nperturbation data cell type?", color = "Network source") +
-      facet_wrap(~perturbation_dataset, scales = "free", ncol = 1) +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))  +
-      xlab("") + 
-      ylab(mymetric)
-    print(plot)
-    ggsave(paste0('timeseries_plots/cell_type_specific_', mymetric, '.svg'), plot, width = 12, height = 12)
-  }
+  X[["metric"]] %<>% factor(levels = gsub("_", " ", METRICS))
+  plot = ggplot(X) +
+    geom_bar(aes(x = regression_method, y = value, fill = regression_method), position = "dodge", stat = "identity") +
+    facet_grid(metric~perturbation_dataset, scales = "free") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))  +
+    ylab("") + 
+    geom_hline(yintercept=0)
+  print(plot)
+  ggsave('timeseries_plots/matching.svg', plot, width = 8, height = 8)
 }
 
-# =========================== Figure 4, perturbation scores and lit reviews for published methods ==========
+
+# =========================== Figure 3, perturbation scores and lit reviews for published methods ==========
 # Definitive Endoderm
 {
   embeddings = load_embeddings("1.5.1_0")
@@ -550,8 +479,8 @@
   ggsave("timeseries_plots/published_methods_definitive_endoderm_lit_review_top_30.pdf", width = 4, height = 6)
 }
 
+# Paul et al. 2015 myelopoiesis data
 {
-  # Paul et al. 2015 myelopoiesis data
   embeddings = load_embeddings("1.5.1_1")
   embeddings$regression_method %<>% gsub("docker____ekernf01/ggrn_docker_backend_", "", .)
   embeddings$regression_method %<>% gsub("dictys", "dictys_dynamics", .)
@@ -648,6 +577,54 @@
     ylab("Number of genes") +
     ggtitle("Literature review of top 30 \npredictions for each model")
   ggsave("timeseries_plots/published_methods_paul_lit_review_top_30.pdf", width = 4, height = 6)
+}
+
+# =========================== Figure 5: cell type-specific networks ===============================
+{
+  X = collect_experiments(paste0("1.4.1_", 0:4))
+  X %<>% subset(is_timescale_strict)
+  X[X[["pearson_top_20"]]!=0, ]
+  X %<>% add_network_cell_type_metadata
+  X %<>% make_the_usual_labels_nice
+  X %<>% subset(is_timescale_strict)
+  X %<>% subset(prediction_timescale==10)
+  X$pearson %<>% tidyr::replace_na(0)
+  X$pearson_top_20 %<>% tidyr::replace_na(0)
+  X$pearson_top_100 %<>% tidyr::replace_na(0)
+  X$pearson_top_200 %<>% tidyr::replace_na(0)
+  X %<>%
+    group_by(prediction_timescale, perturbation_dataset, network_datasets, network_source, network_tissue, network_pretty, network_cell_type_matches ) %>%
+    summarise(across(DEFAULT_METRICS, mean, na.rm = T))
+  X %<>% tidyr::pivot_longer(cols = all_of(DEFAULT_METRICS), names_to = "metric")
+  X[["metric"]] %<>% gsub("_", " ", .)
+  X[["metric"]] %<>% factor(levels = gtools::mixedsort(unique(X[["metric"]])))
+  X %<>% subset(network_source != "humanbase")
+  X$source = X$network_source %>% 
+    gsub("_tissue", "", . ) %>% 
+    gsub("cellnet_human.*", "cellnet_human", .) %>% 
+    gsub("cellnet_mouse.*", "cellnet_mouse", .)
+  for(mymetric in c("overlap top 20", "pearson top 20")){
+    plot = X %>% 
+      subset(metric == mymetric) %>%
+      ggplot() + 
+      geom_bar(
+        aes(
+          x = network_pretty, 
+          y = value, 
+          color = source, 
+          fill = c("Different", "Matched")[network_cell_type_matches+1]
+        ), 
+        stat = "identity", 
+        position = "dodge") +
+      scale_fill_manual(values = c("gray", "black")) +
+      labs(fill = "Network cell type matches \nperturbation data cell type?", color = "Network source") +
+      facet_wrap(~perturbation_dataset, scales = "free", ncol = 1) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))  +
+      xlab("") + 
+      ylab(mymetric)
+    print(plot)
+    ggsave(paste0('timeseries_plots/cell_type_specific_', mymetric, '.svg'), plot, width = 12, height = 12)
+  }
 }
 
 
